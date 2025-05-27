8256c1c8db6cdb5106ee6204b421fb7025aacb58
JOHN CARLOS ARRIETA ARRIETA <jarrietaarrieta@hotmail.com>
2025-05-27 17:20:09 -0500

    feat(account): add mappers for CreateAccountCommand and AccountCreatedResponse

---
+++ account-service/src/main/java/com/cobre/accountservice/application/mapper/AccountMapper.java
@@ -0,0 +1,20 @@
+package com.cobre.accountservice.application.mapper;
+
+import com.cobre.accountservice.application.dto.CreateAccountCommand;
+import com.cobre.accountservice.application.dto.CreateAccountResponse;
+import com.cobre.accountservice.domain.model.Account;
+
+public class AccountMapper {
+
+    public static Account toDomain(CreateAccountCommand request) {
+        return new Account(
+                request.getName(),
+                request.getEmail(),
+                request.getInitialBalance()
+        );
+    }
+
+    public static CreateAccountResponse toResponse(Account account) {
+        return new CreateAccountResponse(account.getId());
+    }
+}
