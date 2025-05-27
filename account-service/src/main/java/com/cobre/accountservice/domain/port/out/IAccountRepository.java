ca28c69db52c450dd849d5d278d7361bff5e130f
JOHN CARLOS ARRIETA ARRIETA <jarrietaarrieta@hotmail.com>
2025-05-27 11:59:19 -0500

    refactor: rename 'ports' to 'port' and add output port for account repository

---
+++ account-service/src/main/java/com/cobre/accountservice/domain/port/out/IAccountRepository.java
@@ -0,0 +1,11 @@
+package com.cobre.accountservice.domain.port.out;
+
+import com.cobre.accountservice.domain.model.Account;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface IAccountRepository {
+    Optional<Account> findById(UUID accountId);
+    void save(Account account);
+}
